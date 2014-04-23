931ce1fa56cd690f4bd9b1c97e063cb28c59f4a1
Ryrzy <ryrzy85@gmail.com>
2014-04-23 19:55:38 +0200

    Revert "PATCH: applied linux kernel patch 3.4.70->3.4.75"
    This reverts commit 97ad2f361d4cc54c8cb528bedb9e006480de7ba4.

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4209,9 +4209,8 @@ static void hub_events(void)
 					hub->hdev->children[i - 1];
 
 				dev_dbg(hub_dev, "warm reset port %d\n", i);
-				if (!udev ||
-				    !(portstatus & USB_PORT_STAT_CONNECTION) ||
-				    udev->state == USB_STATE_NOTATTACHED) {
+				if (!udev || !(portstatus &
+						USB_PORT_STAT_CONNECTION)) {
 					status = hub_port_reset(hub, i,
 							NULL, HUB_BH_RESET_TIME,
 							true);
